0de4de70a8fab980e3c3b2b5e4a3e9b12917c827
Patrick <patrickzhuang@hotmail.fr>
2020-06-17 18:48:56 +0200

    gitignore

---
+++ projetAl1Instagram/Model/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  projetAl1Instagram
+//
+//  Created by Developer on 17/06/2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import Foundation
---
+++ projetAl1Instagram/Service/PostService.swift
@@ -0,0 +1,9 @@
+//
+//  PostService.swift
+//  projetAl1Instagram
+//
+//  Created by Developer on 17/06/2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import Foundation
