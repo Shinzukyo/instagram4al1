5fd1a9f0d9a0ec4a48f4666097756da8a8ab6520
Patrick <patrickzhuang@hotmail.fr>
2020-06-17 15:45:30 +0200

    Initial Commit

---
+++ projetAl1Instagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  projetAl1Instagram
+//
+//  Created by Developer on 17/06/2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
